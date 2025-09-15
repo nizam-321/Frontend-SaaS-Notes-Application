c2cc8a99de15b96a5fbe498981bff7db341ad3dd
nizam-321 <bhainizam303@gmail.com>
2025-09-15 11:51:47 +0530

    change url in Noteform.jsx

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -14,7 +14,7 @@ export default function NoteForm({ token, onNoteCreated }) {
     setError('');
 
     try {
-      const res = await fetch('http://localhost:5000/api/notes', {
+      const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/notes`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
